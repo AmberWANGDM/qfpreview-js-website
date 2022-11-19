a58ee3b073b710fb50812b7064f2065f89c03bc6
WangDanmeng <WangDanmeng98@163.com>
2022-11-19 22:59:57 +0800

    侧边栏权限管理

--- admin/utils/LoadView.js
+++ admin/utils/LoadView.js
@@ -21,6 +21,15 @@ function topBarRender(user) {
   }
 }
 
+// 根据用户角色实现动态渲染侧边栏
+function sideMenuRender(user) {
+  // 权限管理
+  if (JSON.parse(user).role !== 'admin') {
+    // 非管理员无权限管理用户
+    document.querySelector('.user-manage-item').remove()
+  }
+}
+
 // 加载topbar和sidebar
 async function load(id) {
   let user = isLogin()
@@ -37,10 +46,10 @@ async function load(id) {
     let sidemenuText = await fetch(
       '/admin/views/components/sidemenu/index.html'
     ).then((response) => response.text())
-
     document.querySelector('.sidemenu').innerHTML = sidemenuText
     // DONE 实现sidemenu选中高亮
     document.getElementById(id).style.color = '#0d6efd'
+    sideMenuRender(user)
   } else {
     // 退出登录，跳转到登录界面
     location.href = '/admin/views/login/index.html'
