72926e1acf645edef0dc3f8db2fa653aa439977a
WangDanmeng <WangDanmeng98@163.com>
2022-11-22 19:08:10 +0800

    前台-新闻搜索

--- web/views/news/index.js
+++ web/views/news/index.js
@@ -1,3 +1,30 @@
 import { load } from '/web/utils/LoadView.js'
 
 load('topbar-news') // 加载topbar
+
+let listGroup = document.querySelector('.list-group')
+
+search.oninput = async function () {
+  if (!search.value) {
+    listGroup.style.display = 'none'
+    return
+  }
+  // json-server支持模糊查询！url?title_like=1
+  let list = await fetch(
+    `http://localhost:3000/news?title_like=${search.value}`
+  ).then((res) => res.json())
+  listGroup.innerHTML = list
+    .map(
+      (item) => `
+      <li class="list-group-item">
+        <a href="/web/views/detail/index.html?${item.id}">${item.title}</a>
+      </li>
+      `
+    )
+    .join('')
+  listGroup.style.display = 'block'
+}
+
+search.onblur = function () {
+  listGroup.style.display = 'none'
+}
